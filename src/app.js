1f929bf3b7f75c2dad934e02654844ecefc86314
Awadje <michaelawad85@gmail.com>
2017-07-18 11:16:22 +0200

    Basic setup of config, db, middleware and server file

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-
-import config from './config';
-import routes from './routes';
