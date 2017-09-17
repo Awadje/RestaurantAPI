b89079b49c09cda41dc143cfbe1bb8e5efc4ab58
Awadje <michaelawad85@gmail.com>
2017-09-17 22:21:58 +0200

    Refactor to foodtruck added review option to foodtrucks and filter reviews by foodtruck

---
+++ src/controller/foodtruck.js
@@ -0,0 +1,105 @@
+import mongoose from 'mongoose';
+import { Router } from 'express';
+import FoodTruck from '../model/foodtruck';
+import Review from '../model/review';
+import bodyParser from 'body-parser';
+
+
+export default({ config, db }) => {
+  let api = Router();
+
+  api.post('/add', (req, res) => {
+    let newFoodTruck = new FoodTruck();
+    newFoodTruck.name = req.body.name;
+    newFoodTruck.foodtype = req.body.foodtype;
+    newFoodTruck.avgcost = req.body.avgcost;
+    newFoodTruck.geometry.coordinates = req.body.geometry.coordinates;
+
+    newFoodTruck.save(err => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: 'FoodTruck saved sucessfully'});
+    });
+  });
+
+  api.get('/', (req, res) => {
+    FoodTruck.find({}, (err, foodtrucks) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(foodtrucks);
+    });
+  });
+
+  api.get('/:id', (req, res) => {
+    FoodTruck.findById(req.params.id, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(foodtruck);
+    });
+  });
+
+  api.put('/:id', (req, res) => {
+    FoodTruck.findById(req.params.id, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+      foodtruck.name = req.body.name;
+      foodtruck.save(err => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: "FoodTruck info updated"});
+      });
+    });
+  });
+
+  api.delete('/:id', (req, res) => {
+    FoodTruck.remove({
+      _id: req.params.id
+    }, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+        res.json({ message: "FoodTruck Succefully deleted" })
+    });
+  });
+
+  api.post('/reviews/add/:id', (req, res) => {
+    FoodTruck.findById(req.params.id, (err, foodtruck) => {
+      if (err) {
+        res.send(err);
+      }
+      let newReview = new Review();
+
+      newReview.title = req.body.title;
+      newReview.text = req.body.text;
+      newReview.foodtruck = foodtruck._id;
+      newReview.save((err, review) => {
+        if (err) {
+          res.send(err);
+        }
+        foodtruck.reviews.push(newReview);
+        foodtruck.save(err => {
+          if (err) {
+            res.send(err);
+          }
+          res.json({ message: 'Foodtruck review saved!'});
+        });
+      });
+    });
+  });
+
+  api.get('/reviews/:id', (req, res) => {
+    Review.find({foodtruck: req.params.id}, (err, reviews) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(reviews)
+  })
+})
+
+  return api;
+}
